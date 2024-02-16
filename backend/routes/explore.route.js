d0de3b12ed9fe70beff3d9a3c898d0aec88d2627
aryan99601 <aryangupta99601@gmail.com>
2024-02-17 00:55:04 +0530

    package.json and server.js updated

--- backend/routes/explore.route.js
+++ backend/routes/explore.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { ensureAuthenticated } from '../middleware/ensureAuthenticated.js';
-import { explorePopularRepos } from '../controllers/ensureAuthenticated.js';
+import { explorePopularRepos } from '../controllers/explore.controller.js';
 const router = express.Router();
 
 router.get("/repos/:language",ensureAuthenticated,explorePopularRepos)
